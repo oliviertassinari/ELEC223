1ab6da454776b448c3a6ae96e254a4fd2745b9a2
Olivier Tassinari <olivier.tassinari@enst.fr>
2013-10-22 16:10:49 +0200

    correction exo3

--- exercices_compilation/exo2/t.c
+++ exercices_compilation/exo2/t.c
@@ -3,7 +3,7 @@
 
 int32_t x = 34;
 int32_t y;
-const char mesg[] = "Hello World!\n";
+const char * const mesg = "Hello World!\n";
 
 int main()
 {
@@ -14,7 +14,6 @@ int main()
 	z = z + 1;
 	t = y+z;
 
-	printf(mesg);
 	printf(mesg);
 	printf("x = %d, y = %d, z = %d, t = %d\n",
 	       x, y, z, t);
--- exercices_compilation/exo3/hello.c
+++ exercices_compilation/exo3/hello.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void test()
+void testStack()
 {
 	int stack2 = 2;
 	printf("stack2 = %p\n", &stack2);
+}
+
+void testHeap()
+{
+	int * heap2 = malloc(sizeof(int));
+	printf("heap2 = %p\n", heap2);
 }
 
 int main()
@@ -12,12 +18,12 @@ int main()
 	int stack1 = 1;
 	printf("stack1 = %p\n", &stack1);
 
-	test();
+	testStack();
 
 	int * heap1 = malloc(sizeof(int));
-	int * heap2 = malloc(sizeof(int));
+	printf("heap1 = %p\n", heap1);
 
-	printf("heap1 = %p\nheap2 = %p\n", heap1, heap2);
+	testHeap();
 
 	return 0;
 }
