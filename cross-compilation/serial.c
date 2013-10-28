cfb6daf24727afa75a0e8806326c95b48d339af1
Olivier Tassinari <olivier.tassinari@enst.fr>
2013-10-28 11:32:50 +0100

    debut cross compile

---
+++ cross-compilation/serial.c
@@ -0,0 +1,24 @@
+#define UTRSTAT0 (*(volatile uint8_t *)0x01D0010)
+#define UTXH0 (*(volatile uint8_t *)0x01D0020)
+#define URXH0 (*(volatile uint8_t *)0x01D024)
+#define ULCON0 (*(volatile uint8_t *)0x01D0000)
+#define UCON0 (*(volatile uint8_t *)0x01D0004)
+#define UBRDIV0 (*(volatile uint16_t *)0x01D0028)
+
+void serial_init()
+{
+  ULCON0 = 0x3;
+  UBRDIV0 = (uint16_t)(40000000 / (115200 x 16)+0.5 ) -1;
+}
+
+void serial_putc(uint8_t c)
+{
+}
+
+char serial_getc()
+{
+}
+
+void serial_puts(char s)
+{
+}
