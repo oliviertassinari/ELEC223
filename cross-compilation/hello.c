236d6af81cda628e783b9b8284e710caf6b40780
tassinari <olivier.tassinari@telecom-paristech.fr>
2013-10-29 18:56:00 +0100

    debut ldscript

--- cross-compilation/hello.c
+++ cross-compilation/hello.c
@@ -1,9 +1,9 @@
+#include <stdint.h>
 #include "serial.h"
 
 int main()
 {
   serial_init();
-  serial_putc("A");
   serial_puts("Hello World!");
 
   return 1;
--- cross-compilation/serial.c
+++ cross-compilation/serial.c
@@ -1,41 +1,61 @@
 #include <stdint.h>
+#include "serial.h"
 
+#define baudValue (33000000 / (115200 * 16) + 0.5)
+
+// Status register
 #define UTRSTAT0 (*(volatile uint8_t *)0x01D0010)
+
+// Transmission data
 #define UTXH0 (*(volatile uint8_t *)0x01D0020)
+
+// Received data
 #define URXH0 (*(volatile uint8_t *)0x01D024)
+
+// Line control register
 #define ULCON0 (*(volatile uint8_t *)0x01D0000)
+
+// Control register
 #define UCON0 (*(volatile uint16_t *)0x01D0004)
+
+// Baud rate divisior
 #define UBRDIV0 (*(volatile uint16_t *)0x01D0028)
+
+
+/**
+   Functions designed to use the serial port 0
+**/
 
 void serial_init()
 {
-	ULCON0 = 0x3;
-	UCON0 = 0x5;
-	UBRDIV0 = (uint16_t)(33000000 / (115200 * 16)+0.5) - 1;
+  ULCON0 = 0x3;
+  UCON0 = 0x5;
+  UBRDIV0 = (uint16_t)(baudValue) - 1;
 }
 
 void serial_putc(uint8_t c)
 {
-	while(UTRSTAT0 & (1 << 1) == 0){}
-
-	UTXH0 = c;
+  // We wait an empty buffer
+  while(UTRSTAT0 & (1 << 1) == 0){}
+  UTXH0 = c;
 }
 
 uint8_t serial_getc()
 {
-
+  // We wait data
+  while(UTRSTAT0 & (1 << 0) == 0){}
+  return URXH0;
 }
 
 void serial_puts(char string[])
 {
-    char charCurrent = 0;
-    int i = 0;
+  char charCurrent = 0;
+  int i = 0;
 
-    do
+  do
     {
-        charCurrent = string[i];
-        i++;
-        serial_putc(charCurrent);
+      charCurrent = string[++i];
+      serial_putc(charCurrent);
     }
-    while(charCurrent != '\0');
+  while(charCurrent != '\0');
 }
