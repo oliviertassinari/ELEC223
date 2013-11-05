f3e31c60e95a0431d31021267789b0b9bedc2293
tassinari <olivier.tassinari@telecom-paristech.fr>
2013-11-05 18:25:29 +0100

    add comment

--- cross-compilation/hello.c
+++ cross-compilation/hello.c
@@ -1,11 +1,14 @@
 #include "serial.h"
 
 int bssvalue1;
-int bssvalue2;
+char bssvalue2;
 int data1 = 5;
+char data2 = '5';
 
 int main()
 {
+  data1 = 1;
+
   serial_init();
   serial_puts("Hello World!");
 
--- cross-compilation/init.c
+++ cross-compilation/init.c
@@ -1,13 +1,14 @@
 #include <stdint.h>
 
-extern uint32_t _bss_start, _bss_end;
+// BSS segment start and end
+extern uint8_t _bss_start, _bss_end;
 
 void init()
 {
-  uint32_t *p1 = &_bss_start;
-  uint32_t *p2 = &_bss_end;
-
   // BSS segment initialization
+  uint8_t *p1 = &_bss_start;
+  uint8_t *p2 = &_bss_end;
+
   while(p1 < p2)
     *p1++ = 0;
 }
