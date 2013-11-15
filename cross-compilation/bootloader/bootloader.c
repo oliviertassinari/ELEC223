8d438dff7fcb3d3ac3952103d203131d851398c8
tassinari <olivier.tassinari@telecom-paristech.fr>
2013-11-15 11:19:40 +0100

    Converteur ascii hex

--- cross-compilation/bootloader/bootloader.c
+++ cross-compilation/bootloader/bootloader.c
@@ -1,6 +1,40 @@
+#include <stdint.h>
 #include "serial.h"
 #include "led.h"
 #include "timer.h"
+
+char hexToAscii(uint8_t h)
+{
+  if(h < 10)
+    {
+      return h + '0';
+    }
+  else
+    {
+      return (h - 10) + 'A';
+    }
+}
+
+uint8_t asciiToHex(char a)
+{
+  if(a <= '9')
+    {
+      return a - '0';
+    }
+  else if(a <= 'F')
+    {
+      return a - 'A' + 10;
+    }
+  else if(a <= 'f')
+    {
+      return a - 'a' + 10;
+    }
+  else
+    {
+      return 0;
+    }
+}
+
 
 int main()
 {
@@ -21,6 +55,8 @@ int main()
       if(instruction_ct < 12)
         {
           serial_putc(getc);
+          serial_putc(hexToAscii(asciiToHex(getc)));
+
           instruction[instruction_ct] = getc;
           instruction_ct += 1;
         }
@@ -28,9 +64,9 @@ int main()
         {
           instruction_ct = 0;
           instruction[12] = '\0';
-          serial_puts("\n\rCommande : ");
+          serial_puts("\n\rCommande : '");
           serial_puts(instruction);
-          serial_puts("\n\r");
+          serial_puts("'\n\r");
         }
     }
 
