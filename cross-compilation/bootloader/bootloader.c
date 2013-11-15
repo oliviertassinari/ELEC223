921a43202c84296d67f02c5c52ba0f138f4931d4
tassinari <olivier.tassinari@telecom-paristech.fr>
2013-11-15 11:22:57 +0100

    Converteur ascii hex

--- cross-compilation/bootloader/bootloader.c
+++ cross-compilation/bootloader/bootloader.c
@@ -52,21 +52,21 @@ int main()
     {
       getc = serial_getc();
 
-      if(instruction_ct < 12)
-        {
-          serial_putc(getc);
-          serial_putc(hexToAscii(asciiToHex(getc)));
-
-          instruction[instruction_ct] = getc;
-          instruction_ct += 1;
-        }
-      else if(getc == '\n' || getc == '\r')
+      if(getc == '\n' || getc == '\r')
         {
           instruction_ct = 0;
           instruction[12] = '\0';
           serial_puts("\n\rCommande : '");
           serial_puts(instruction);
           serial_puts("'\n\r");
+        }
+      else if(instruction_ct < 12)
+        {
+          serial_putc(getc);
+          serial_putc(hexToAscii(asciiToHex(getc)));
+
+          instruction[instruction_ct] = getc;
+          instruction_ct += 1;
         }
     }
 
