c55e759e3326d73cc6078e8a2157a9541b6cf2df
Olivier Tassinari <olivier.tassinari@enst.fr>
2013-11-16 16:26:30 +0100

    Fix bug compile

--- cross-compilation/lib/timer.h
+++ cross-compilation/lib/timer.h
@@ -1,6 +1,33 @@
 #ifndef TIMER_H
 #define TIMER_H
 
+// TCFG0 Configures the three 8-bit prescalers
+#define TCFG0 (*(volatile uint32_t *)0x01D50000)
+
+// TCFG1 6-MUX & DMA mode selecton register
+#define TCFG1 (*(volatile uint32_t *)0x01D50004)
+
+// TCON Timer control register
+#define TCON (*(volatile uint32_t *)0x01D50008)
+
+// TCNTB0 Timer 0 count buffer register
+#define TCNTB0 (*(volatile uint32_t *)0x01D5000C)
+
+// TCMPB0 Timer 0 compare buffer register
+#define TCMPB0 (*(volatile uint32_t *)0x01D50010)
+
+// TCNTO0 Timer 0 count observation register
+#define TCNTO0 (*(volatile uint32_t *)0x01D50014)
+
+// TCNTB0 Timer 3 count buffer register
+#define TCNTB3 (*(volatile uint32_t *)0x01D50030)
+
+// TCMPB0 Timer 3 compare buffer register
+#define TCMPB3 (*(volatile uint32_t *)0x01D50034)
+
+// TCNTO0 Timer 3 count observation register
+#define TCNTO3 (*(volatile uint32_t *)0x01D50038)
+
 void timer_start(int time);
 void timer_stop();
 void timer_sleep(int time);
